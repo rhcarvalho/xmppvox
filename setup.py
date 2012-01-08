c9393a9ff79bff0d31f7294b08a2ec7ff8607ebb
Rodolfo Carvalho <rhcarvalho@gmail.com>
2012-01-08 13:57:20 -0200

    Adiciona setup.py para ser usado com o py2exe*.
    Nota: o PyInstaller é preferível ao py2exe por gerar um arquivo único sem dependência de DLLs.

---
+++ setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    version = '0.1.0',
+    name = 'XMPPVOX',
+    description = 'XMPPVOX',
+
+    console = ['xmppvox.py'],
+    options = {
+        'py2exe': {
+            'bundle_files': 1,
+            'compressed': True,
+            'packages': [
+                'dns',
+                'sleekxmpp',
+            ],
+            'includes': [
+                #'sleekxmpp.features.feature_starttls',
+                #'sleekxmpp.features.feature_bind',
+                #'sleekxmpp.features.feature_session',
+                #'sleekxmpp.features.feature_mechanisms',
+                #'sleekxmpp.plugins.base',
+                #'sleekxmpp.plugins.xep_0030',
+                #'sleekxmpp.plugins.xep_0004',
+                #'sleekxmpp.plugins.xep_0060',
+                #'sleekxmpp.plugins.xep_0199',
+                'xml.etree.ElementTree',
+            ],
+            'excludes': [
+                'calendar',
+                'difflib',
+                'doctest',
+                'inspect',
+                'locale',
+                'pdb',
+                'pickle',
+                'pyreadline',
+                'unittest',
+            ],
+        },
+    },
+    zipfile = None,
+)
