a1696ba0b8624cd8c2adea10f9641c009b31fe24
Rodolfo Carvalho <rhcarvalho@gmail.com>
2012-01-20 18:32:22 -0200

    Adiciona docstring ao módulo de testes.

--- test.py
+++ test.py
@@ -18,6 +18,12 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+u"""
+XMPPVOX - módulo de testes
+
+Este módulo implementa testes automatizados para o XMPPVOX.
+"""
+
 import unittest
 import socket
 import sys
