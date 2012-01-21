f7172d5421cca888865c8341be54caf8579779a6
Rodolfo Carvalho <rhcarvalho@gmail.com>
2012-01-21 01:48:57 -0200

    Melhora documentação e remove código para executar o módulo client.py de forma independente.

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
-﻿#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 #    XMPPVOX: XMPP client for DOSVOX.
 #    Copyright (C) 2012  Rodolfo Henrique Carvalho
@@ -55,26 +54,16 @@ class BotXMPP(sleekxmpp.ClientXMPP):
     é chamada e a mensagem é passada como argumento.
     É possível fornecer sua própria função para tratar mensagens.
     """
-    # Accept and create bidirectional subscription requests:
+    # Aceita e cria requisições de inscrição bidirecional:
     auto_authorize = True
     auto_subscribe = True
 
     def __init__(self, jid, password):
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
 
-        # The session_start event will be triggered when
-        # the bot establishes its connection with the server
-        # and the XML streams are ready for use. We want to
-        # listen for this event so that we we can initialize
-        # our roster.
+        # Eventos do SleekXMPP que serão tratados
         self.add_event_handler("session_start", self.start)
-
-        # The message event is triggered whenever a message
-        # stanza is received. Be aware that that includes
-        # MUC messages and error messages.
         self.add_event_handler("message", self.message)
-
-        # Outros eventos interessantes
         self.add_event_handler('got_online', self.got_online)
         self.add_event_handler('got_offline', self.got_offline)
         self.add_event_handler('changed_status', self.changed_status)
@@ -96,21 +85,19 @@ class BotXMPP(sleekxmpp.ClientXMPP):
         self.register_plugin('xep_0199') # XMPP Ping
 
     def start(self, event):
-        """
-        Process the session_start event.
+        u"""Processa evento de início de sessão.
 
-        Typical actions for the session_start event are
-        requesting the roster and broadcasting an initial
-        presence stanza.
-
-        Arguments:
-            event -- An empty dictionary. The session_start
-                     event does not provide any additional
-                     data.
+        Quando uma conexão é estabelecida, a lista de contatos é solicitada.
         """
         self.get_roster()
 
     def papovox_connected(self, event):
+        u"""Processa evento de conexão com o Papovox.
+
+        Quando o Papovox se conecta ao XMPPVOX, o usuário envia a presença
+        inicial para o servidor e passa a estar efetivamente online para
+        seus contatos.
+        """
         self.nickname = event.get('nick')
         self.message_handler = event.get('message_handler')
         # Envia presença em broadcast. Neste momento o usuário aparece como
@@ -118,19 +105,18 @@ class BotXMPP(sleekxmpp.ClientXMPP):
         self.send_presence(pnick=self.nickname)
 
     def papovox_disconnected(self, event):
+        u"""Processa evento de desconexão do Papovox.
+
+        Quando o Papovox se desconecta do XMPPVOX, a conexão com o servidor
+        XMPP é encerrada.
+        """
         self.disconnect()
 
     def message(self, msg):
-        """
-        Process incoming message stanzas. Be aware that this also
-        includes MUC messages and error messages. It is usually
-        a good idea to check the messages's type before processing
-        or sending replies.
+        u"""Processa mensagens recebidas.
 
-        Arguments:
-            msg -- The received message stanza. See the documentation
-                   for stanza objects and the Message stanza to see
-                   how it may be used.
+        As mensagens podem ser também de bate-papo multi-usuário (MUC)
+        ou mensagens de erro.
         """
         # Só processa mensagens depois que 'message_handler' for definido.
         # Ignora mensagens que não sejam 'chat' ou 'normal', por exemplo
@@ -140,12 +126,23 @@ class BotXMPP(sleekxmpp.ClientXMPP):
             self.talking_to = msg['from']
 
     def got_online(self, presence):
+        u"""Registra que um contato apareceu online."""
         log.debug(u"Entrou: %s" % presence['from'])
 
     def got_offline(self, presence):
+        u"""Registra que um contato ficou offline."""
         log.debug(u"Saiu: %s" % presence['from'])
 
     def changed_status(self, presence):
+        u"""Processa evento de presenças de contatos.
+
+        Quando um contato envia uma nova presença, seu estado pode ter mudado
+        de diferentes formas. Ele pode exibir uma nova mensagem de status,
+        mudar para disponível/ocupado/ausente/etc, etc.
+
+        Caso ele mude de apelido, este apelido será usado na interface do
+        XMPPVOX.
+        """
         # A presença pode ou não incluir um elemento 'nick'.
         # Em ambos os casos o comando abaixo executa corretamente, retornando
         # None caso 'nick' não esteja presente.
@@ -193,66 +190,3 @@ class BotXMPP(sleekxmpp.ClientXMPP):
             # Ou, se for uma string, remove resource (tudo depois da /, inclusive):
             bare_jid = jid_obj_or_string.split('/', 1)[0]
         return bare_jid
-
-
-if __name__ == '__main__':
-    # Setup the command line arguments.
-    optp = OptionParser()
-
-    # Output verbosity options.
-    optp.add_option('-q', '--quiet', help='set logging to ERROR',
-                    action='store_const', dest='loglevel',
-                    const=logging.ERROR, default=logging.INFO)
-    optp.add_option('-d', '--debug', help='set logging to DEBUG',
-                    action='store_const', dest='loglevel',
-                    const=logging.DEBUG, default=logging.INFO)
-    optp.add_option('-v', '--verbose', help='set logging to COMM',
-                    action='store_const', dest='loglevel',
-                    const=5, default=logging.INFO)
-
-    # JID and password options.
-    optp.add_option("-j", "--jid", dest="jid",
-                    help="JID to use")
-    optp.add_option("-p", "--password", dest="password",
-                    help="password to use")
-
-    opts, args = optp.parse_args()
-
-    # Setup logging.
-    logging.basicConfig(level=opts.loglevel,
-                        format='%(levelname)-8s %(message)s')
-
-    if opts.jid is None:
-        opts.jid = raw_input("Username: ")
-    if opts.password is None:
-        opts.password = getpass.getpass("Password: ")
-
-    # Setup the GenericBot and register plugins. Note that while plugins may
-    # have interdependencies, the order in which you register them does
-    # not matter.
-    xmpp = GenericBot(opts.jid, opts.password)
-    xmpp.register_plugin('xep_0030') # Service Discovery
-    xmpp.register_plugin('xep_0004') # Data Forms
-    xmpp.register_plugin('xep_0060') # PubSub
-    xmpp.register_plugin('xep_0199') # XMPP Ping
-
-    # If you are working with an OpenFire server, you may need
-    # to adjust the SSL version used:
-    # xmpp.ssl_version = ssl.PROTOCOL_SSLv3
-
-    # If you want to verify the SSL certificates offered by a server:
-    # xmpp.ca_certs = "path/to/ca/cert"
-
-    # Connect to the XMPP server and start processing XMPP stanzas.
-    if xmpp.connect():
-        # If you do not have the dnspython library installed, you will need
-        # to manually specify the name of the server if it does not match
-        # the one in the JID. For example, to use Google Talk you would
-        # need to use:
-        #
-        # if xmpp.connect(('talk.google.com', 5222)):
-        #     ...
-        xmpp.process(block=True)
-        print("Done")
-    else:
-        print("Unable to connect.")
