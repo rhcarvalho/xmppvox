a69174171b398bc1333da8b5c75d028f6555b40e
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-10-08 16:38:14 +0800

    Handle exceptions when closing sockets that might be already closed.

--- src/server.py
+++ src/server.py
@@ -133,10 +133,16 @@ class PapovoxLikeServer(object):
     def disconnect(self):
         u"""Desliga a conexão com o Papovox."""
         log.debug(u"Encerrando conexão com o Papovox...")
-        self.socket.shutdown(socket.SHUT_RDWR)
-        self.socket.close()
-        self.server_socket.shutdown(socket.SHUT_RDWR)
-        self.server_socket.close()
+        try:
+            self.socket.shutdown(socket.SHUT_RDWR)
+            self.socket.close()
+        except socket.error, e:
+            log.debug("Client socket: %s" % e)
+        try:
+            self.server_socket.shutdown(socket.SHUT_RDWR)
+            self.server_socket.close()
+        except socket.error, e:
+            log.debug("Server socket: %s" % e)
 
     # Funções de integração com o cliente XMPP --------------------------------#
 
